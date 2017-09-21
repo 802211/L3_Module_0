003cb0c161fed09b138000903f08fe533a46ff30
FruitWave <league@WTS8.attlocal.net>
2017-09-20 18:28:04 -0700

    Finished array lists, look at stacks
    Finished array lists, look at stacks

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,11 +1,16 @@
 package IntroToArrayLists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+public class GuestBook implements ActionListener{
+	 // Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
@@ -14,20 +19,53 @@ public class GuestBook {
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
 	
-	
+	JButton View;
+	JButton Add;
+	int x = 0;
 	void setup() {
 		JFrame f = new JFrame();
 		JPanel p = new JPanel();
-		JButton View = new JButton();
-		JButton Add = new JButton();
+	View = new JButton();
+	Add = new JButton();
 		f.add(p);
+		f.setSize(500, 300);
 		p.add(Add);
 		p.add(View);
+		View.setText("View Names");
+		Add.setText("Add Name");
+	View.addActionListener(this);;
+		Add.addActionListener(this);
 		f.setVisible(true);
 		View.setSize(200, 100);
 		Add.setSize(200, 100);
+		
 	}
-	
+	public static void main(String[] args) {
+		GuestBook gb = new GuestBook();
+		gb.setup();
+	}
+	ArrayList<String> array = new ArrayList<String>();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String add = "";
+		if(e.getSource() == Add){
+			add = JOptionPane.showInputDialog("Enter a Name.");
+		
+		array.add(add);
+		
+			
+		}
+		if(e.getSource() == View){
+			String c = "";
+			for(String s: array){
+				x = x+1;
+				c += "Guest #" + x + ":" + s + "\n";
+			}
+		
+			JOptionPane.showMessageDialog(null, c);
+		}
+	}
 	
 	
 }
