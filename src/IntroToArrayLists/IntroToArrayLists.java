9999b136260cbc405f87f4e3f2a57a5e3a4c9ba4
FruitWave <league@WTS8.attlocal.net>
2017-09-27 18:31:03 -0700

    IntroToArrayLists(IP), TextUndoRedo(IP), IntroToStack

--- src/IntroToArrayLists/IntroToArrayLists.java
+++ src/IntroToArrayLists/IntroToArrayLists.java
@@ -1,20 +1,42 @@
 package IntroToArrayLists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList <String> array = new ArrayList <String>();
 		//2. Add five Strings to your list
+		array.add("a123");
+		array.add("b123");
+		array.add("c123");
+		array.add("d123");
+		array.add("e123");
 		
 		//3. Print all the Strings using a standard for-loop
-		
+		System.out.println("Print all the Strings using a standard for-loop");
+		int x = array.size();
+		for(int i = 0; i<x; i++){
+			System.out.println(array.get(i));
+		}
 		//4. Print all the Strings using a for-each loop
-		
+		System.out.println("Print all the Strings using a for-each loop");
+for(String y: array){
+	System.out.println(y);
+}
 		//5. Print only the even numbered elements in the list.
-		
+System.out.println("Print only the even numbered elements in the list");
+				for(int j = 0; j<x; j++){
+			if(j % 2 == 0){
+			System.out.println(array.get(j));
+		}}
 		//6. Print all the Strings in reverse order.
-		
+				System.out.println("Print all the Strings in reverse order");
+		for(int a = x-1; a>-1; a--){
+			System.out.println(array.get(a));
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		System.out.println("Print only the Strings that have the letter 'e' in them.");
 	}
 }
--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,17 +1,74 @@
 package IntroToStacks;
 
-public class TextUndoRedo {
-	/* 
+import java.awt.Event;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.security.Key;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TextUndoRedo implements KeyListener {
+	/*
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
-	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
+	 * Every time a key is pressed, add that character to the JLabel. It should
+	 * look like a basic text editor.
 	 * 
-	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
-	 * Save that deleted character onto a Stack of Characters.
+	 * Make it so that every time the BACKSPACE key is pressed, the last
+	 * character is erased from the JLabel. Save that deleted character onto a
+	 * Stack of Characters.
 	 * 
-	 * Choose a key to be the Undo key. Make it so that when that key is pressed, the top Character is popped 
-	 * off the Stack and added back to the JLabel.
+	 * Choose a key to be the Undo key. Make it so that when that key is
+	 * pressed, the top Character is popped off the Stack and added back to the
+	 * JLabel.
 	 * 
-	 * */
+	 */
+	Stack <char> label = new Stack <char>();
+	JFrame f;
+	JPanel p;
+	JLabel l;
+
+	public static void main(String[] args) {
+		TextUndoRedo TUR = new TextUndoRedo();
+		TUR.setup();
+
+	}
 	
+	void setup() {
+
+		f = new JFrame();
+		p = new JPanel();
+		l = new JLabel();
+		f.add(p);
+		p.add(l);
+		f.setVisible(true);
+		f.setSize(500, 500);
+		f.addKeyListener(this);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+			
+		if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE){
+			
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
 }
